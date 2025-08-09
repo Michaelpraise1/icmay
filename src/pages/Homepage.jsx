ac647be3a179c5a1838a6adeff9452ba61deef44
Michaelpraise1 <michaeld56567@gmail.com>
2025-08-08 19:12:49 -0700

    thrid

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,11 +3,10 @@ import React from "react";
 import Herosection from "../component/Herosection";
 import Services from "../component/Services";
 import Stat from "../component/Stat";
-
 import { FloatButton } from "antd";
 import Weem from "../component/Weem";
 import Hero from "../component/Hero";
-import Bottom from "../component/bottom";
+import Bottom from "../component/Bottom";
 
 const Homepage = () => {
   return (
