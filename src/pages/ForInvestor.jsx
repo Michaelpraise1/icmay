bf8a93b802204975a3004677a52c9d21c2133be6
Michaelpraise1 <michaeld56567@gmail.com>
2025-09-25 11:13:50 -0700

    bjgg

--- src/pages/ForInvestor.jsx
+++ src/pages/ForInvestor.jsx
@@ -24,13 +24,14 @@ const ForInvestor = () => {
       </div>
       <div className='flex flex-col items-center space-y-3'>
         <h2 className='text-2xl font-semibold'>Our Promise</h2>
-        <p className='text-center'>As a member of our investor network, you can expect:</p>
+        <p className='text-center'>As a member of our investor network, you can expect</p>
 
         <h2 className='text-xl text-center font-semibold'>Curated and tailored Opportunities:</h2>
         <p className='text-center'>We will bring you investment opportunities that fit your preferred industry, phase of business, and geographical area. These opportunities are always curated and checked by our team.</p>
         
-        <h2 className='text-xl font-semibold'>Efficient Matchmaking:</h2>
-        <p className='text-center'>Benefit from our 25+ years of experience in fundraising and investment banking.</p>
+        <h2 className='text-xl font-semibold'>Efficient Matchmaking</h2>
+        <p className='text-center mb-5'>Benefit from our 25+ years of experience in fundraising and investment banking.</p>
+        
 
         <h2 className='text-2xl font-semibold'>Trustworthy Interactions</h2>
         <p className='text-center'>Our team stands for transparency, honesty, and integrity. We work tirelessly to create meaningful relationships between investors and entrepreneurs.</p>
