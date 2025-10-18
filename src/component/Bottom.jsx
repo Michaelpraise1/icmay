692f5ba1578e66188eb1c99889978e772659f39e
Michaelpraise1 <michaeld56567@gmail.com>
2025-10-17 21:21:15 -0700

    omo

--- src/component/Bottom.jsx
+++ src/component/Bottom.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Bottom = () => {
   return (
-      <div className=" bg-[#22272a]   text-center">
+      <div className="bg-[#22272a] text-center">
         <p>&copy; {new Date().getFullYear()} ICMA Fundación. All rights reserved.</p>
       </div>
   )
